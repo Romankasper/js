ed124a7dab63771f40c16e3bedad897928aae4e5
Romankasper <roman_kaspryshyn@ukr.net>
2022-02-02 09:20:17 +0200

    added homework

---
+++ leson1/hw.js
@@ -0,0 +1,98 @@
+//Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let greeting = 'hello';
+// let xxx ='owu';
+// let yyy = 'com';
+// let zzz = 'ua';
+// let number = 1;
+// let a = 10;
+// let b = -999;
+// let c = 123;
+// let d = 3.14;
+// let e = 2.7;
+// let f = 16;
+// let g = 10 > 5;
+// let h = 5 < 1;
+//Вивести кожну змінну за допомогою: console.log , alert, document.write
+// document.write(`<div>${greeting}</div>`)
+// alert(xxx)
+// console.log(yyy)
+// alert(zzz)
+// console.log(number)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+//Переприсвоїти кожній змінній з завдання значення на довільне
+// greeting = 'good morning'
+// xxx = 'black'
+// yyy = 'food'
+// zzz = 'color'
+// namber = 2123
+// a = 20
+// b = 999
+// c = 3
+// d = 6
+// e = 'write'
+// f = 10
+// g = 13
+// h = 9/3
+// document.write(`<div>${e}</div>`)
+//Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(xxx)
+// console.log(yyy)
+// alert(zzz)
+// console.log(number)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(greeting)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+//Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let firstName = 'Roman'
+// let middleName = 'Stepanovich'
+// let lastName = 'Kaspryshyn'
+// let person = firstName + middleName + lastName
+// console.log(person)
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// const name =prompt('what is your name?')
+// const lastname = prompt('What is your lastname?')
+// const age = prompt('How old are you?')
+// const result = 'hello ' + name + ' ' + lastname + ' you are ' + age + ' years old'
+// console.log(result)
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консолm
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// В однакових виразах не використовувати однакові оператори!!!
+// let a = 5 < 6
+// console.log(a)
+// let b = 5 > 6
+// console.log(b)
+// console.log(5 === 6 )
+// console.log(5 == 6 )
+// console.log(10 == 10)
+// console.log(10 === 10)
+// console.log(10 > 10)
+// console.log(10 < 10)
+// console.log(10 !== 10)
+// console.log(123 !== 123)
+// console.log(123 <= 123)
+// Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+// let str = "20"
+//  let a = 5
+//  document.write(str + a + "<br/>"); 205
+//  document.write(str - a + "<br/>"); 15
+//  document.write(str * a + "<br/>"); 100
+//  document.write(str / str + "<br/>");1
