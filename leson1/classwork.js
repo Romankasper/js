3517e5215e90aa1575e10ed27436023217da15dd
Romankasper <roman_kaspryshyn@ukr.net>
2022-02-02 18:42:02 +0200

    added classwork

---
+++ leson1/classwork.js
@@ -0,0 +1,70 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let a = [1, 2, 3, 4, 43, 67, 91 , 637 ,34.45, 43];
+//  let resolt = (previousValue, currentValue) => previousValue + currentValue;
+//  console.log(a.reduce(resolt));
+
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book = {
+//    title:'451 degrees Fahrenheit',
+//     number: 501,
+//     genre:'anti-utopia'
+// }
+// console.log(book)
+
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// let book = {
+//    title: '1984',
+//    number:484,
+//    genre:'anti-utopia',
+//    author:'George Orwell'
+// }
+// console.log(book)
+
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+//  let book1 ={
+//      title: '1984',
+//      number:484,
+//      genre:'anti-utopia',
+//      author:'George Orwell'
+//  }
+// let book2 ={
+//     title: '451 degrees Fahrenheit',
+//     number:501,
+//     genre:'anti-utopia',
+//     author:'Ray Bradbury'
+// }
+// let book3 ={
+//     title: 'alchemist ',
+//     number:345,
+//     genre:'novel',
+//     author:'Paulo Coelho '
+// }
+// let books = [book1, book2, book3]
+// console.log(books[0])
+// console.log(books[1])
+// console.log(books[2])
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s
+// let height = 23
+// let width = 10
+// let s =(height*width)
+// console.log(s)
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+// let heightC = 10
+// let dC = 4
+// const PI = 3.14
+// let v =(PI*dC*heightC)
+// console.log(v)
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+// let n = 3
+// let m = 4
+// let k =(Math.pow(3,2)+Math.pow(4,2))
+// console.log(k)
--- leson1/hw.js
+++ leson1/hw.js
@@ -1,22 +1,24 @@
 //Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
-// let greeting = 'hello';
-// let xxx ='owu';
-// let yyy = 'com';
-// let zzz = 'ua';
-// let number = 1;
-// let a = 10;
-// let b = -999;
-// let c = 123;
-// let d = 3.14;
-// let e = 2.7;
-// let f = 16;
-// let g = 10 > 5;
-// let h = 5 < 1;
+ let greeting = 'hello';
+ let xxx ='owu';
+ let yyy = 'com';
+ let zzz = 'ua';
+ let number = 1;
+ let a = 10;
+ let b = -999;
+ let c = 123;
+ let d = 3.14;
+ let e = 2.7;
+ let f = 16;
+ let g = 10 > 5;
+ let h = 5 < 1;
+
+
 //Вивести кожну змінну за допомогою: console.log , alert, document.write
-// document.write(`<div>${greeting}</div>`)
-// alert(xxx)
+// console.log(greeting)
+// console.log(xxx)
 // console.log(yyy)
-// alert(zzz)
+// console.log(zzz)
 // console.log(number)
 // console.log(a)
 // console.log(b)
@@ -26,6 +28,36 @@
 // console.log(f)
 // console.log(g)
 // console.log(h)
+
+ // alert(greeting)
+ // alert(xxx)
+ // alert(yyy)
+ // alert(zzz)
+ // alert(number)
+ // alert(a)
+ // alert(b)
+ // alert(c)
+ // alert(d)
+ // alert(e)
+ // alert(f)
+ // alert(g)
+ // alert(h)
+
+ // document.write(greeting)
+ // document.write(`<h1>${xxx}</h1>`)
+ // document.write(`<div>${yyy}</div>`)
+ // document.write(zzz + "<br/>")
+ // document.write(number + "<br/>")
+ // document.write(a)
+ // document.write(b)
+ // document.write(c)
+ // document.write(d)
+ // document.write(e)
+ // document.write(f)
+ // document.write(g)
+ // document.write(h)
+
+
 //Переприсвоїти кожній змінній з завдання значення на довільне
 // greeting = 'good morning'
 // xxx = 'black'
@@ -41,6 +73,7 @@
 // g = 13
 // h = 9/3
 // document.write(`<div>${e}</div>`)
+
 //Вивести кожну змінну за допомогою: console.log , alert, document.write
 // console.log(xxx)
 // console.log(yyy)
@@ -54,19 +87,25 @@
 // console.log(f)
 // console.log(g)
 // console.log(h)
+
+
 //Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
 // let firstName = 'Roman'
 // let middleName = 'Stepanovich'
 // let lastName = 'Kaspryshyn'
 // let person = firstName + middleName + lastName
 // console.log(person)
+
+
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
 // Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
-// const name =prompt('what is your name?')
-// const lastname = prompt('What is your lastname?')
-// const age = prompt('How old are you?')
-// const result = 'hello ' + name + ' ' + lastname + ' you are ' + age + ' years old'
-// console.log(result)
+//  const name =prompt('what is your name?')
+//  const lastname = prompt('What is your lastname?')
+//  const age = prompt('How old are you?')
+//  const result = 'hello ' + name + ' ' + lastname + ' you are ' + age + ' years old'
+//  document.write(`<h1>${result}</h1>`)
+
+
 //- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консолm
 // let a = 100;
 // let b = '100';
@@ -74,6 +113,8 @@
 // console.log(typeof a)
 // console.log(typeof b)
 // console.log(typeof c)
+
+
 // Визначити відповідний оператор в виразах що б вийшов відповідний результат.
 // В однакових виразах не використовувати однакові оператори!!!
 // let a = 5 < 6
@@ -89,6 +130,8 @@
 // console.log(10 !== 10)
 // console.log(123 !== 123)
 // console.log(123 <= 123)
+
+
 // Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
 // let str = "20"
 //  let a = 5
